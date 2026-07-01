c090c81a1c8e40f873223d8e18c101513de04128
Ion Obreja <jo@da.md>
2026-07-01 17:04:01 +0300

    fix: correct site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://joSecret.github.io',
+  site: 'https://josecret.github.io/',
   base: '/bots',
 });
